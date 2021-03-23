37dc38a9b40c5f654da08c143787c79ee4040ecd
changmyeong <changmyeong.baek@gmail.com>
2021-03-23 19:34:30 +0900

    fix: add user in order hash feat: add events in exchange feat: add cancelOrder

--- contracts/NFTFarmExchange.sol
+++ contracts/NFTFarmExchange.sol
@@ -12,7 +12,7 @@ struct Order {
     address target;
     bytes targetCalldata;
     address paymentToken;
-    uint256 paymentAmount;
+    uint256 priceAmount;
     uint256 feePercent;
     uint256 expirationBlock;
     uint256 nonce;
@@ -33,8 +33,8 @@ contract NFTFarmExchange is Ownable {
     // proxy for send nft
     mapping(address => DelegateProxy) public proxies;
 
-    event Exchange(address nft, address seller, address buyer, address paymentToken, uint256 paymentAmount);
-    event CancelOrder();
+    event Exchange(bytes32 sellHash, bytes32 buyHash, address paymentToken, uint256 priceAmount, uint256 paymentAmount, uint256 feeAmount);
+    event CancelOrder(bytes32 indexed orderHash);
 
     constructor(address _team, uint256 _minimumFeePercent) {
         team = _team;
@@ -45,7 +45,7 @@ contract NFTFarmExchange is Ownable {
         address _target,
         bytes calldata _targetCalldata,
         address _paymentToken,
-        uint256 _paymentAmount,
+        uint256 _priceAmount,
         uint256 _feePercent,
         address _seller,
         address _buyer,
@@ -55,20 +55,20 @@ contract NFTFarmExchange is Ownable {
     ) public {
         // prevent stack limit error
         _exchangeTargetForToken(
-            Order(_seller, _target, _targetCalldata, _paymentToken, _paymentAmount, _feePercent, _expirationBlocks[0], _nonces[0]),
+            Order(_seller, _target, _targetCalldata, _paymentToken, _priceAmount, _feePercent, _expirationBlocks[0], _nonces[0]),
             _signatures[0],
-            Order(_buyer, _target, _targetCalldata, _paymentToken, _paymentAmount, _feePercent, _expirationBlocks[1], _nonces[1]),
+            Order(_buyer, _target, _targetCalldata, _paymentToken, _priceAmount, _feePercent, _expirationBlocks[1], _nonces[1]),
             _signatures[1]
         );
     }
 
     function _exchangeTargetForToken(Order memory _sellOrder, bytes memory _sellerSignature, Order memory _buyOrder, bytes memory _buyerSignature) internal {
-        bytes32 sellHash = keccak256(abi.encodePacked(_sellOrder.target, _sellOrder.targetCalldata, _sellOrder.paymentToken, _sellOrder.paymentAmount, _sellOrder.feePercent, _sellOrder.expirationBlock, _sellOrder.nonce));
+        bytes32 sellHash = keccak256(abi.encodePacked(_sellOrder.user, _sellOrder.target, _sellOrder.targetCalldata, _sellOrder.paymentToken, _sellOrder.priceAmount, _sellOrder.feePercent, _sellOrder.expirationBlock, _sellOrder.nonce));
         require(_validHash(_sellOrder.user, sellHash, _sellerSignature), 'invalid seller signature');
         require(closedOrders[sellHash] == false, 'closed seller order');
         require(_sellOrder.expirationBlock == 0 || _sellOrder.expirationBlock < block.timestamp, 'expired seller order');
 
-        bytes32 buyHash = keccak256(abi.encodePacked(_buyOrder.target, _buyOrder.targetCalldata, _buyOrder.paymentToken, _buyOrder.paymentAmount, _buyOrder.feePercent, _buyOrder.expirationBlock, _buyOrder.nonce));
+        bytes32 buyHash = keccak256(abi.encodePacked(_buyOrder.user, _buyOrder.target, _buyOrder.targetCalldata, _buyOrder.paymentToken, _buyOrder.priceAmount, _buyOrder.feePercent, _buyOrder.expirationBlock, _buyOrder.nonce));
         require(_validHash(_buyOrder.user, buyHash, _buyerSignature), 'invalid buyer signature');
         require(closedOrders[buyHash] == false, 'closed buyer order');
         require(_buyOrder.expirationBlock == 0 || _buyOrder.expirationBlock < block.timestamp, 'expired buyer order');
@@ -77,13 +77,15 @@ contract NFTFarmExchange is Ownable {
         require(_matchOrder(_sellOrder, _buyOrder), 'not matched order');
 
         require(_buyOrder.feePercent >= minimumFeePercent, 'fee percent too low');
-        uint256 paymentAmount = _buyOrder.paymentAmount;
-        uint256 feeAmount = paymentAmount.div(100).mul(_buyOrder.feePercent);
-
+        uint256 priceAmount = _buyOrder.priceAmount;
+        uint256 feeAmount = priceAmount.div(100).mul(_buyOrder.feePercent);
+        uint256 paymentAmount = priceAmount.sub(feeAmount);
+        
+        address paymentToken = _buyOrder.paymentToken;
         DelegateProxy buyerProxy = proxies[_buyOrder.user];
-        require(buyerProxy.proxyTransferFrom(_buyOrder.paymentToken, _buyOrder.user, _sellOrder.user, paymentAmount.sub(feeAmount)), 'failed to send payment amount');
+        require(buyerProxy.proxyTransferFrom(paymentToken, _buyOrder.user, _sellOrder.user, paymentAmount), 'failed to send payment amount');
         if (feeAmount > 0) {
-            require(buyerProxy.proxyTransferFrom(_buyOrder.paymentToken, _buyOrder.user, team, paymentAmount.sub(feeAmount)), 'failed to send fee');
+            require(buyerProxy.proxyTransferFrom(paymentToken, _buyOrder.user, team, feeAmount), 'failed to send fee');
         }
 
         DelegateProxy sellerProxy = proxies[_sellOrder.user];
@@ -91,6 +93,26 @@ contract NFTFarmExchange is Ownable {
 
         closedOrders[sellHash] = true;
         closedOrders[buyHash] = true;
+
+        emit Exchange(sellHash, buyHash, paymentToken, priceAmount, paymentAmount, feeAmount);
+    }
+
+    function cancelOrder(
+        address _target,
+        bytes calldata _targetCalldata,
+        address _paymentToken,
+        uint256 _priceAmount,
+        uint256 _feePercent,
+        address _user,
+        uint256 _expirationBlock,
+        uint256 _nonce,
+        bytes calldata _signature
+    ) public {
+        bytes32 orderHash = keccak256(abi.encodePacked(_user, _target, _targetCalldata, _paymentToken, _priceAmount, _feePercent, _expirationBlock, _nonce));
+        require(_validHash(_user, orderHash, _signature), 'invalid signature');
+        require(closedOrders[orderHash] == false, 'closed seller order');
+        closedOrders[orderHash] = false;
+        emit CancelOrder(orderHash);
     }
 
     function _validHash(address _signer, bytes32 _message, bytes memory _signature) internal pure returns(bool) {
@@ -102,7 +124,7 @@ contract NFTFarmExchange is Ownable {
         return orderA.target == orderB.target &&
                _equalBytes(orderA.targetCalldata, orderB.targetCalldata) &&
                orderA.paymentToken == orderB.paymentToken &&
-               orderA.paymentAmount == orderB.paymentAmount &&
+               orderA.priceAmount == orderB.priceAmount &&
                orderA.feePercent == orderB.feePercent;
     }
 
